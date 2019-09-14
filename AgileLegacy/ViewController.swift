b6e2b63a06d05545c3af89b8375288c3a2405136
Desmay Kevin <kevin.desmay@gmail.com>
2019-09-14 23:19:26 +0200

    Initial Commit

---
+++ AgileLegacy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AgileLegacy
+//
+//  Created by Kevin Desmay on 14/09/2019.
+//  Copyright © 2019 Kevin Desmay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
